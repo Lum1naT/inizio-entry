d8d7c5d3c65033512fb0cc406383aa267cc0fb49
Lum1naT <47953544+Lum1naT@users.noreply.github.com>
2020-10-09 17:20:25 +0200

    Update databaseManager.php

--- databaseManager.php
+++ databaseManager.php
@@ -1,12 +1,13 @@
 <?php
-require '\Doctrine\DBAL\DriverManager';
+// namespace \vendor;
+use \Doctrine\DBAL\DriverManager as ORM;
 
 echo "include succ.";
 
 $connectionParams = array(
     'url' => $_ENV('CLEARDB_DATABASE_URL'),
 );
-$conn = DriverManager::getConnection($connectionParams);
+$conn = ORM::getConnection($connectionParams);
 
 if(!empty($conn)){
     echo "Connected succ.";
