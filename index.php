025cba2ef15439269f81518bd042fe0e45c5cd52
Lum1naT <47953544+Lum1naT@users.noreply.github.com>
2020-10-11 16:34:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -18,31 +18,12 @@
 
     ?>
 
-    <form>
+    <form action="/formHandler.php" method="post">
     <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
     <input name="ico" class="form-control" type="text" pattern="[0-9]+" id="ico" placeholder="Zadejte IČO">
     </div>
-    <div class="form-group">
-    <label for="exampleInputEmail1">DIČ</label>
-    <input name="dic" class="form-control" type="text">
-    </div>
-    <div class="form-group">
-    <label for="exampleInputEmail1">Firma</label>
-    <input name="firma" class="form-control" type="text">
-    </div>
-    <div class="form-group">
-    <label for="exampleInputEmail1">Ulice</label>
-    <input name="ulice" class="form-control" type="text">
-    </div>
-    <div class="form-group">
-    <label for="exampleInputEmail1">Město</label>
-    <input name="mesto" class="form-control" type="text">
-    </div>
-    <div class="form-group">
-    <label for="exampleInputEmail1">PSC</label>
-    <input name="psc" class="form-control" type="text">
-    </div>
+    <button type="submit" class="btn btn-primary">Hledat</button>
     
     </form>
 
@@ -66,35 +47,6 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 
-    <script type='text/javascript'>
-
-$(document).ready(function(){
- $('#ico').change( function() {
-   var ico = $(this).val();
-   $.ajax({
-    type: "POST",
-    url: "/formHandler.php",
-    contentType: "application/json; charset=utf-8",
-    dataType: "json", 
-    data: "ico="+ico,
-    cache: false,
-    success: function(data) {
-     if (data.stav == 'ok') {
-      $('input[name=dic]').val(data.dic);
-      $('input[name=firma]').val(data.firma);
-      $('input[name=ulice]').val(data.ulice);
-      $('input[name=mesto]').val(data.mesto);
-      $('input[name=psc]').val(data.psc);
-      alert('Název a sídlo firmy bylo vyplněno z databáze ARES.' + data.firma);
-     } else {
-      alert(data.stav);
-     }
-    },
-   });
-  });
-});
-
-  </script>
 
   </body>
 </html>
