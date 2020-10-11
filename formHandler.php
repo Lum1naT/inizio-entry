fc00f07fe81da4dd185f88ff6491b4f14b0c8155
Lum1naT <47953544+Lum1naT@users.noreply.github.com>
2020-10-11 22:27:22 +0200

    Update formHandler.php

--- formHandler.php
+++ formHandler.php
@@ -17,7 +17,10 @@ $a = array();
 if ($xml) {
  $ns = $xml->getDocNamespaces();
  $data = $xml->children($ns['are']);
- if(!empty(strval($data->children($ns['D'])->E))){ $_SESSION["error"] = 'IČ firmy nebylo nalezeno';  header('Location: https://vast-garden-09239.herokuapp.com/?orderBy=published&page=1'); echo "OH NO!"; die;}
+ if(!empty(strval($data->children($ns['D'])->E))){ 
+    $_SESSION["error"] = 'IČ firmy nebylo nalezeno';  
+    header('Location: https://vast-garden-09239.herokuapp.com/?orderBy=published&page=1'); 
+    die;}
 
  $el = $data->children($ns['D'])->VBAS;
  if (strval($el->ICO) == $ico) {
