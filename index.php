261f541b77b2b7a0444dfa335e0607823078e542
Lum1naT <47953544+Lum1naT@users.noreply.github.com>
2020-10-12 22:03:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 if(empty($_GET['orderBy'])){
-  header('Location: https://vast-garden-09239.herokuapp.com/?orderBy=name&page=1');
+  header('Location: https://vast-garden-09239.herokuapp.com/?orderBy=published&page=1');
 }
 session_start();
 require_once('./vendor/autoload.php');
