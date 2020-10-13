38c250a5f4ee9360fcb952c6b27dc9a7305d5989
Lum1naT <47953544+Lum1naT@users.noreply.github.com>
2020-10-13 09:16:35 +0200

    Update formHandler.php

--- formHandler.php
+++ formHandler.php
@@ -60,7 +60,7 @@ try {
 
 
     $count = $conn->insert('firma', array('ico' => $a['ico'], 
-                                            'published' => date("DD-MM-YYYY H:i:s",$t),
+                                            'published' => date("d-m-Y H:i:s",$t),
                                             'dic' => $a['dic'],                                           
                                             'name' => $a['name'],
                                             'street' => $a['street'],
